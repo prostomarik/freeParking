ea95e813aef0287b84b7562a1f00946167385e38
vasiliy566 <vasyaisaev31@gmail.com>
2019-11-30 13:32:06 +0300

    remove usless detector

--- cars_personss_detection/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-pip3 install --user numpy
-pip3 install --user argparse
-pip3 install --user opencv-python
-pip3 install --user imutils
--- cars_personss_detection/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python3 real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
-
