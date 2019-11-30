db045fcc62a68738333b8159030c8a14d01dd7ef
vasiliy566 <vasyaisaev31@gmail.com>
2019-11-30 12:55:09 +0300

    add model that detected cars from recent project

---
+++ cars_personss_detection/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update
+pip3 install --user numpy
+pip3 install --user argparse
+pip3 install --user opencv-python
+pip3 install --user imutils
---
+++ cars_personss_detection/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+
